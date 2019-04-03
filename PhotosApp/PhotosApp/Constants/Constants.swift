4fb17457ba530e3b531b30aecb4732ea1bd95898
rohity821 <rohity821@gmail.com>
2019-04-03 23:43:27 +0530

    added constants to project

---
+++ PhotosApp/PhotosApp/Constants/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  PhotosApp
+//
+//  Created by Rohit Yadav on 03/04/19.
+//  Copyright © 2019 Rohit Yadav. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let cellIdentifier = "imageCell"
+    static let photoBrowserSegue = "openPhotoBrowser"
+    static let placeholderImage = "placeholder"
+    static let photoListCellHeight  = 114.0
+}
